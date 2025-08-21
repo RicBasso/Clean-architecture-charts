d31230aba665de7a0d3f4f8d61520caf0619de24
Ricky <basso.riccardo@yahoo.it>
2025-08-21 12:10:11 +0200

    created the 4 layers

--- clean_architecture_chart.py
+++ clean_architecture_chart.py
@@ -6,11 +6,18 @@ colors = ["#FFD700", "#ADFF2F", "#87CEFA", "#FF6347"]
 
 canvas, charts = plt.subplots(1, 2, figsize=(12, 6), subplot_kw={'aspect': 'equal'})
 
-def create_chart(chart):
-    chart.pie([1], radius = 1 )
+size = 0.25
+
+def create_chart(chart, labels):
+    count = 0
+    for label in reversed(labels):
+        chart.pie([1], radius = 1 - count * size, colors=[colors[len(labels)-1-count]],wedgeprops=dict(width=size, edgecolor='w'))
+        count = count + 1
+     
+    
 
 
 
-create_chart(charts[0])
-create_chart(charts[1])
+create_chart(charts[0], original)
+create_chart(charts[1], mapped)
 plt.show()
