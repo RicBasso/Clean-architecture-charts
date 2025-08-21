6e1b9ede00d3f79737296f652204bea3d86247d7
Ricky <basso.riccardo@yahoo.it>
2025-08-21 11:57:18 +0200

    insert arrays of values and colors

---
+++ clean_architecture_chart.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+original = ["Entities", "Use Cases", "Interface Adapters", "Frameworks & Drivers"]
+mapped = ["Domain", "Application", "Presentation", "Infrastructure"]
+colors = ["#FFD700", "#ADFF2F", "#87CEFA", "#FF6347"]
+
