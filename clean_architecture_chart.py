43917a0809c395524dbf0524ab83554b7a457989
Ricky <basso.riccardo@yahoo.it>
2025-08-21 11:58:20 +0200

    Created the base of the charts

--- clean_architecture_chart.py
+++ clean_architecture_chart.py
@@ -4,3 +4,13 @@ original = ["Entities", "Use Cases", "Interface Adapters", "Frameworks & Drivers
 mapped = ["Domain", "Application", "Presentation", "Infrastructure"]
 colors = ["#FFD700", "#ADFF2F", "#87CEFA", "#FF6347"]
 
+canvas, charts = plt.subplots(1, 2, figsize=(12, 6), subplot_kw={'aspect': 'equal'})
+
+def create_chart(chart):
+    chart.pie([1], radius = 1 )
+
+
+
+create_chart(charts[0])
+create_chart(charts[1])
+plt.show()
